aaebb7a3eb9dd6ddf4080aeeff272b3fb313e33d
alexcuatepitzirojas <74808877+alexcuatepitzirojas@users.noreply.github.com>
2022-11-29 17:56:42 -0600

    Add files via upload

---
+++ boolean.php
@@ -0,0 +1,33 @@
+<?php
+
+$pedro  =true;
+$juan   = true;
+$sergio = true;
+$yahir = false;
+if($pedro == true){
+echo "<p>Usted tiene autorizacion</p>";
+}
+
+if($juan == true){
+
+echo"<p>Usted tiene la autorizacion</p>";
+
+}
+
+
+if($sergio == true)
+{
+
+echo "<p>Usted tiene autorizacion</p> ";
+
+}
+
+if($yahir == false)
+{
+
+echo "<p>Usted no tiene Autorizacion</p>";
+
+}
+
+?>
+
