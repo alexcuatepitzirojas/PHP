c993e656fe5df7e32356cbffad038919772d7fed
alexcuatepitzirojas <74808877+alexcuatepitzirojas@users.noreply.github.com>
2022-11-29 18:02:55 -0600

    Add files via upload

---
+++ boolean.php
@@ -0,0 +1,34 @@
+<?php
+
+$pedro  =true;
+$juan   = true;
+$sergio = true;
+$yahir = false;
+//* Esta es una forma de usa los valores boolean en PHP **//
+if($pedro == true){
+echo "<p>Usted tiene autorizacion</p>";
+}
+
+if($juan == true){
+
+echo"<p>Usted tiene la autorizacion</p>";
+
+}
+
+
+if($sergio == true)
+{
+
+echo "<p>Usted tiene autorizacion</p> ";
+
+}
+
+if($yahir == false)
+{
+
+echo "<p>Usted no tiene Autorizacion</p>";
+
+}
+
+?>
+
