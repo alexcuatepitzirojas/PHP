d0172f8ee539d639b78eb4a39fe9e0420328445f
alexcuatepitzirojas <74808877+alexcuatepitzirojas@users.noreply.github.com>
2022-11-30 16:40:36 -0600

    Add files via upload

---
+++ cadenalogica.php
@@ -0,0 +1,17 @@
+<?php
+
+$cadena = "";
+
+if($cadena == true ){
+
+echo "<p>Una Cadena No Vacia Se Considera Como True</p>";
+
+}
+
+if($cadena == false ){
+
+echo "<p>Una Cadena Vacia Se Considera Como False</p>";
+
+}
+
+?>
